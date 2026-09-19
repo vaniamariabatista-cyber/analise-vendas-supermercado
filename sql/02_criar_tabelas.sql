627ad7ac0b91e1bc09b2b00b67443a90d9de6e21
vaniamariabatista-cyber <vaniamariabatista@gmail.com>
2026-09-19 20:04:19 -0300

    feat(sql): cria tabela raw_vendas

---
+++ sql/02_criar_tabelas.sql
@@ -0,0 +1,19 @@
+CREATE TABLE raw_vendas (
+    "Invoice ID" TEXT,
+    "Branch" TEXT,
+    "City" TEXT,
+    "Customer type" TEXT,
+    "Gender" TEXT,
+    "Product line" TEXT,
+    "Unit price" TEXT,
+    "Quantity" TEXT,
+    "Tax 5%" TEXT,
+    "Sales" TEXT,
+    "Date" TEXT,
+    "Time" TEXT,
+    "Payment" TEXT,
+    "cogs" TEXT,
+    "gross margin percentage" TEXT,
+    "gross income" TEXT,
+    "Rating" TEXT
+);
