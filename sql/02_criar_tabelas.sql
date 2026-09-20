23f5871064be0c52616ae5fc552e346e99291e00
vaniamariabatista-cyber <vaniamariabatista@gmail.com>
2026-09-20 19:53:35 -0300

    feat: cria tabela vendas_tratadas e renomeia scripts conforme o enunciado

--- sql/02_criar_tabelas.sql
+++ sql/02_criar_tabelas.sql
@@ -1,4 +1,4 @@
-CREATE TABLE raw_vendas (
+CREATE TABLE IF NOT EXISTS raw_vendas (
     "Invoice ID" TEXT,
     "Branch" TEXT,
     "City" TEXT,
@@ -16,4 +16,24 @@ CREATE TABLE raw_vendas (
     "gross margin percentage" TEXT,
     "gross income" TEXT,
     "Rating" TEXT
+);
+
+CREATE TABLE IF NOT EXISTS vendas_tratadas (
+    id_venda VARCHAR(50) PRIMARY KEY NOT NULL,
+    filial VARCHAR(10) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    tipo_cliente VARCHAR(50),
+    genero VARCHAR(20),
+    linha_produto VARCHAR(150) NOT NULL,
+    preco_unitario NUMERIC(10,2) CHECK (preco_unitario >= 0),
+    quantidade INTEGER CHECK (quantidade > 0),
+    imposto NUMERIC(10,2) CHECK (imposto >= 0),
+    valor_total NUMERIC(12,2) CHECK (valor_total >= 0),
+    data_venda DATE,
+    hora_venda TIME,
+    forma_pagamento VARCHAR(50) NOT NULL,
+    custo_mercadoria NUMERIC(12,2) CHECK (custo_mercadoria >= 0),
+    margem_percentual NUMERIC(10,2),
+    receita_bruta NUMERIC(12,2) CHECK (receita_bruta >= 0),
+    avaliacao NUMERIC(4,2) CHECK (avaliacao >= 0 AND avaliacao <= 10)
 );
